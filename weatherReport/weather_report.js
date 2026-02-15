a031d9a01da08a27f6a54f2dd00431345c623c96
JhoAlexander <jhoa.alexander@icloud.com>
2026-02-15 13:31:23 -0500

    Utilizar la funcion FETCH en javascript

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,26 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = '6ece00bc8aa272652ac9065af12d'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) throw new Error('Ciudad no encontrada'); 
+      return response.json();
+    })
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      // Es una buena práctica usar textContent o limpiar antes de asignar HTML
+      weatherInfo.innerHTML = `<h2>Clima en ${data.name}</h2>
+                               <p>Temperatura: ${data.main.temp} °C</p>
+                               <p>Clima: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>No se encontraron datos, intenta otra vez por fa</p>`;
+      });
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
